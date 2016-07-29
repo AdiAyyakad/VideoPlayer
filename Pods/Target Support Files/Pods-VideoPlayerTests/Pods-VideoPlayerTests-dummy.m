63d90c66be6488b6a0e7d6d2498489ece7caaa81
njaditya <njaditya@gmail.com>
2016-07-29 17:38:35 -0400

    Deintegrated unused CocoaPods

--- Pods/Target Support Files/Pods-VideoPlayerTests/Pods-VideoPlayerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VideoPlayerTests : NSObject
-@end
-@implementation PodsDummy_Pods_VideoPlayerTests
-@end
--- Pods/Target Support Files/Pods-VideoPlayerTests/Pods-VideoPlayerTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_VideoPlayerTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VideoPlayerTestsVersionString[];
-
